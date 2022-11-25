d82e8051c93a1d1b0f11256b0b031a69b921b54d
Luca Bomben <luca.bomben@gmail.com>
2022-11-25 15:30:54 +0100

    Added files to initial OCP install configuration

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ ansible-playbook tasks/configure_bind_dns.yml
 ansible-playbook tasks/configure_httpd.yml
 ansible-playbook tasks/configure_tftp_pxe.yml
 ansible-playbook tasks/configure_haproxy_lb.yml
-
+ansible-playbook tasks/configure_ocp.yml
